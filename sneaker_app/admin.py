711c6f89095dc86d43b3816aa10c2542526ba9a0
knockoutko <kyle.christian.ko@gmail.com>
2021-05-25 12:18:39 -0700

    added admin capabilities

--- sneaker_app/admin.py
+++ sneaker_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import User, Api, Review
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Api)
+class ApiAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    pass
+
 # Register your models here.
