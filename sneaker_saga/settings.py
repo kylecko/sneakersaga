4c3b4be0812e92242387991d73e79b1a19ce4ca9
knockoutko <kyle.christian.ko@gmail.com>
2021-06-03 13:34:39 -0700

    allowed hosts

--- sneaker_saga/settings.py
+++ sneaker_saga/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rh-2pwh21-&332!xw%vo151_!tid4&jg$qu1b)e0zbptia#*ch'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.117.145.31']
 
 
 # Application definition
